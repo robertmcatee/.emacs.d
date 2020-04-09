f365b21a55ddc6e741d4c603a0047b9f7a7954a6
Robert McAtee <rtmcatee@gmail.com>
2020-04-09 13:05:59 -0500

    Initial commit

---
+++ init.el
@@ -0,0 +1,44 @@
+;; Look and Feel
+(load-theme 'tango-dark)
+(set-frame-parameter (selected-frame) 'alpha '(85 50))
+(add-to-list 'default-frame-alist '(alpha 85 50))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Lucida Console" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
+
+;; MELPA
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(package-initialize)
+
+;; Markdown Mode
+(unless (package-installed-p 'markdown-mode)
+  (package-refresh-contents)
+  (package-install 'markdown-mode))
+(require 'markdown-mode)
+
+;; Babel
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '(
+   (python .t)
+   )
+ )
+
+;; Variables Bucket
+(setq
+ default-directory "/home/robert"
+ inhibit-startup-message '(t)
+ initial-scratch-message "Listo"
+ initial-major-mode 'org-mode
+ org-startup-indented '(t)
+ package-selected-packages 'markdown-mode)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote markdown-mode)))
