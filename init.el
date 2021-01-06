25b618121670d655251d5cbce1c4febc8386c7be
Robert McAtee <robert_mcatee@txwb.uscourts.gov>
2021-01-06 11:54:18 -0600

    MELPA Changes , Windows Tweaks/Additions

--- init.el
+++ init.el
@@ -1,18 +1,23 @@
 ;; Look and Feel
 (load-theme 'tango-dark)
-(set-frame-parameter (selected-frame) 'alpha '(85 50))
-(add-to-list 'default-frame-alist '(alpha 85 50))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Liberation Mono" :foundry "outline" :slant normal :weight normal :height 90 :width normal)))))
+(set-frame-parameter (selected-frame) 'alpha '(95 50))
+(add-to-list 'default-frame-alist '(alpha 95 50))
 
-;; MELPA
-(require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(package-initialize)
+;; Fonts
+(custom-set-faces
+ ;; '(default ((t (:family "Liberation Mono" :foundry "outline" :slant normal :weight normal :height 90 :width normal))))) ;; Linux
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 100 :width normal))))) ;; Windows
+
+;; load emacs 24's package system. Add MELPA repository.
+(when (>= emacs-major-version 24)
+  (require 'package)
+    (add-to-list
+      'package-archives
+      ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
+      ;;'("melpa" . "http://melpa.milkbox.net/packages/") t
+      '("melpa" . "https://melpa.org/packages/") t
+    )
+)
 
 ;; Markdown Mode
 (unless (package-installed-p 'markdown-mode)
@@ -30,7 +35,8 @@
 
 ;; Variables Bucket
 (setq
- default-directory "/home/robert"
+ ;; default-directory "/home/robert" ;; Linux
+ default-directory "C:\Users\robertmcatee\source" ;; Windows
  inhibit-startup-message '(t)
  initial-scratch-message "Listo"
  initial-major-mode 'org-mode
@@ -43,7 +49,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote markdown-mode))
+ '(package-selected-packages 'markdown-mode)
  '(scroll-bar-mode nil)
  '(tool-bar-mode nil))
 
