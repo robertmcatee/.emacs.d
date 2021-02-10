e28992bff753bf97aec941dd6ebb6e6c69c36fd3
Robert McAtee <robert_mcatee@txwb.uscourts.gov>
2021-02-10 10:53:40 -0600

    changed color theme

--- init.el
+++ init.el
@@ -1,19 +1,3 @@
-;; Look and Feel
-(load-theme 'tsdh-dark)
-(set-frame-parameter (selected-frame) 'alpha '(95 95))
-(add-to-list 'default-frame-alist '(alpha 95 95))
-
-;; Fonts
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 140 :width normal)))))
- ;; Windows
- ;;'(default ((t (:family "Liberation Mono" :foundry "outline" :slant normal :weight normal :height 90 :width normal)))))
- ;; Linux
-
 ;; load emacs 24's package system. Add MELPA repository.
 (when (>= emacs-major-version 24)
   (require 'package)
@@ -24,6 +8,12 @@
       '("melpa" . "https://melpa.org/packages/") t
     )
 )
+
+;; Solarized Dark Theme
+(unless (package-installed-p 'color-theme-sanityinc-solarized)
+  (package-refresh-contents)
+  (package-install 'color-theme-sanityinc-solarized))
+(require 'color-theme-sanityinc-solarized)
 
 ;; Markdown Mode
 (unless (package-installed-p 'markdown-mode)
@@ -51,6 +41,23 @@
   (package-install 'tabbar))
 (require 'tabbar)
 
+;; Look and Feel
+;; (load-theme 'tsdh-dark)
+(load-theme 'sanityinc-solarized-dark)
+(set-frame-parameter (selected-frame) 'alpha '(95 95))
+(add-to-list 'default-frame-alist '(alpha 95 95))
+
+;; Fonts
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Consolas" :foundry "outline" :slant normal :weight normal :height 140 :width normal)))))
+ ;; Windows
+ ;;'(default ((t (:family "Liberation Mono" :foundry "outline" :slant normal :weight normal :height 90 :width normal)))))
+ ;; Linux
+
 ;; Babel
 (org-babel-do-load-languages
  'org-babel-load-languages
@@ -77,11 +84,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" default))
  '(package-selected-packages '(tabbar ox-mdx-deck ox-jekyll-md markdown-mode))
  '(scroll-bar-mode nil)
- '(tool-bar-mode nil)
- '(tabbar-mode 1)
- '(tabbar-buffer-groups-function (lambda () (list "All"))))
+ '(tabbar-buffer-groups-function (lambda nil (list "All")) t)
+ '(tabbar-mode 1 nil (tabbar))
+ '(tool-bar-mode nil))
 
 (set-background-color "#16161D")
 ;; (global-set-key (kbd "<scroll>") nil)
