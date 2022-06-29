84c00c98d658e1393b184796b3ce53bd1fed52d3
Robert McAtee <rtmcatee@gmail.com>
2022-06-29 11:49:36 -0500

    Update init.el

--- init.el
+++ init.el
@@ -257,10 +257,3 @@
                   ("C-c n t" . org-roam-tag-add)
                   ("C-c n a" . org-roam-alias-add)
                   ("C-c n l" . org-roam-buffer-toggle)))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(org-roam use-package undo-tree treemacs-tab-bar treemacs-projectile treemacs-persp treemacs-magit treemacs-icons-dired treemacs-evil tabbar solarized-theme ox-haunt lsp-ui geiser-guile)))
